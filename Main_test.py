9103d68d148531f45bdde424d22961562e764992
Maximilian <kuechle@stud.uni-heidelberg.de>
2023-01-24 22:18:28 +0100

    update main

--- Main_test.py
+++ Main_test.py
@@ -733,8 +733,9 @@ def testDataSets(method="sca-DomainAdaption", beta=1.0, delta=1.0, n=10):
     X = []
     Y = []
 
-    for rot, scale, shear in [(0, 1.0, 0.0), (3.14, 1.0, 0.0), (0, 10, 0.0), (3.14, 10, 0.0), (0.0, 1.0, 5.0),
-                              (3.14, 1.0, 5.0)]:
+    for counter, tp in enumerate([(0, 1.0, 0.0), (3.14, 1.0, 0.0), (0, 10, 0.0), (3.14, 10, 0.0), (0.0, 1.0, 5.0),
+                              (3.14, 1.0, 5.0)]):
+        rot, scale, shear = tp
         data.twoDomains2_roate(n=50, rot=rot, scale=scale, shear=shear)
         g = []
         for i in range(data.X.shape[0]):
@@ -744,7 +745,7 @@ def testDataSets(method="sca-DomainAdaption", beta=1.0, delta=1.0, n=10):
         g = np.median(g)
 
         #for gamma in [0.1, 0.5, 1.0, 10.0]:
-        for couner, gamma in enumerate([0.1, 0.3, 1.0, 3, 10.0]):
+        for gamma in [0.1, 0.3, 1.0, 3, 10.0]:
             # for gamma in [0.1, 0.2]:
             # lda = MyKerneLDA(n_components=2, kernel=kernel, gamma=gamma)
             # lda = MyKernelPCA(n_components=2, kernel=kernel, gamma=gamma)
