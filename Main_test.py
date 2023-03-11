92a40a267540e608fdf419a14f410a512c150c62
Maximilian <kuechle@stud.uni-heidelberg.de>
2023-03-11 20:16:50 +0100

    changed Main_test

--- Main_test.py
+++ Main_test.py
@@ -1333,7 +1333,7 @@ if __name__ == '__main__':
         for beta in [0.0, 0.25, 0.5, 1.0]:
             for delta in [0, 0.25, 0.5, 1.0]:
         #        print("beta ", beta, " delta ", delta, " centering ")
-        #        test_split_V3("sca-DomainGeneralization", beta=beta, delta=delta,
-        #                                                  centering=ce)
+                test_split_V3("sca-DomainGeneralization", beta=beta, delta=delta,
+                                                          centering=ce)
                 test_split_V3("sca-DomainAdaption", beta=beta, delta=delta,
                                                           centering=ce)
