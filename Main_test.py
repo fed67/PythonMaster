18afc90ae957373fcf16e29e08f1c134046d0184
Maximilian <kuechle@stud.uni-heidelberg.de>
2023-03-13 10:52:59 +0100

    changed Main_test

--- Main_test.py
+++ Main_test.py
@@ -513,7 +513,7 @@ def test_LDA_Sklearn_split_treatment_dimension(method="kda", centering=True, bet
                                           [*titles[0:9]], [inv_map] * (len(y_train_list) + 0),
                                           title_fig="{1}-{0}-Center {2}- Train V1,V2,V3 Test V4 ".format(kern, name,
                                                                                                          centering),
-                                          domainNames=["V1", "V2", "V3", "V4"], path="graphics/v1v2v3v4")
+                                          domainNames=["V1", "V2", "V3", "V4"], path="graphics/v1v2v3v4/")
     plt.figtext(0.5, 0.01,
                 "UMAP Plot\nDimension of train data: rows: {0}; features: {1}\n sample: {2}".format(X_train.shape[0],
                                                                                                     X_test.shape[1],
